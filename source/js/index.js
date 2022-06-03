03d8d3cef5abe9209e28b425d639b0d181f7f933
“Aliosha796” <1806730767@qq.com>
2022-06-03 21:15:52 +0800

    删去了视频背景，为名言增加了打字机效果

--- source/js/index.js
+++ source/js/index.js
@@ -1,8 +1,27 @@
 const word = document.querySelector(".home-font");
+const span = document.querySelector("#word")
+const background = document.querySelector("#background")
+/* 打字机效果函数 */
+function typing(str) {
+    console.log(str);
+    let newStr = '';
+    let i = 0;
+    let timer = setInterval(() => {
+        if (newStr.length < str.length) {
+            newStr += str[i++];
+            span.innerHTML = newStr + '_';
+
+        } else {
+            clearInterval(timer)
+            span.innerHTML = newStr
+        }
+    }, 200)
+}
+
 fetch('https://v1.hitokoto.cn?c=i&c=k')
     .then(res => res.json())
     .then(res => {
-        console.log(res)
-        console.log(res.hitokoto);
-        word.innerHTML = res.hitokoto 
+        /* span.innerHTML = res.hitokoto */
+        typing(res.hitokoto)
     });
+
